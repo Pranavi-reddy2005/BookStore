e63cfec5bbca607516cd337af0f38f3d00f1ef92
Pranavi-reddy2005 <ammireddypranavi35@gmail.com>
2024-11-17 14:49:55 +0530

    Navbar Coded Added

--- MyProject/store-client/src/components/Home/RecentlyAdded.jsx
+++ MyProject/store-client/src/components/Home/RecentlyAdded.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const RecentlyAdded = () => {
   return (
-    <div>RecentlyAdded</div>
+    <div className='mt-8 px-4'>
+      <h4 className='text-3xl text-yellow-100'>Recently added books</h4>
+    </div>
   )
 }
 
