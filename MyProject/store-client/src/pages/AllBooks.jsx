34f3f2927a8201b4d11c8cb25d5a37af841c748f
Pranavi-reddy2005 <ammireddypranavi35@gmail.com>
2024-11-17 14:52:35 +0530

    All Books Code Added

--- MyProject/store-client/src/pages/AllBooks.jsx
+++ MyProject/store-client/src/pages/AllBooks.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Loader2 } from 'lucide-react'
@@ -35,14 +35,5 @@ const allBooks = () => {
 }
 
 export default allBooks
-=======
-import React from 'react'
 
-const AllBooks = () => {
-  return (
-    <div>AllBooks</div>
-  )
-}
 
-export default AllBooks
->>>>>>> 01279ba012121f8b0cb37650d67b65aa9e8283f0
