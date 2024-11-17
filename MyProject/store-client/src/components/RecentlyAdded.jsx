1622ceecb5274497263aa6475f247154cabd2d84
Shailaja-2 <167770048+Shailaja-2@users.noreply.github.com>
2024-11-17 14:56:30 +0530

    Recently Added Code Edited

--- MyProject/store-client/src/components/RecentlyAdded.jsx
+++ MyProject/store-client/src/components/RecentlyAdded.jsx
@@ -8,7 +8,7 @@ const RecentlyAdded = () => {
         const fetch = async () => {
             const response = await axios.get(
                 " "
-            )
+            ) 
         }
         fetch()
     }, [])
