1f5492b669dfb3ca409ec8df47cd0d042a97eb93
NikithaDevasoth <devasothnikitha09@gmail.com>
2024-11-17 23:50:03 +0530

    BooksRoute completed

--- Server/server-mongoose/routes/BookRoute.js
+++ Server/server-mongoose/routes/BookRoute.js
@@ -26,4 +26,64 @@ router.post("/addbook",authenticateToken,async(req,res)=>{
         return res.status(500).json({message:"internal server error"})
     }
 });
+router.put("/update-book",authenticateToken,async(req,res)=>{
+    try{
+        const{bookid}=req.headers;
+        await Book.findByIdAndUpdate(bookid,{
+           
+                url:req.body.url,
+                title:req.body.title,
+                author:req.body.author,
+                price:req.body.price,
+                desc:req.body.desc,
+                language:req.body.language,
+            });
+            return res.status(200).json({message:"Book Updated Successfully!"});
+        }catch(error){
+            console.log(error);
+            return res.status(500).json({message:"An error Occurred"})
+
+    }
+});
+router.delete("/delete-book",authenticateToken,async(req,res)=>{
+    try{
+
+        const {bookid}=req.headers;
+        await Book.findByIdAndDelete(bookid);
+        return res.status(200).json({message:"Book deleted Successfully!",});
+        
+    }catch(error){
+        return res.status(500).json({message:"An error occurred"});
+    }
+});
+router.get("/get-all-books",async(req,res)=>{
+    try{
+        const books=await Book.find().sort({createdAt:-1});
+        return res.json({status:"Success",data:books,});
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:"An error occurred"})
+    }
+    
+});
+router.get("/get-recent-books",async(req,res)=>{
+    try{
+        const books=await Book.find().sort({createdAt:-1}).limit(4);
+        return res.json({status:"Success",data:books,});
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:"An error occurred"})
+    }
+    
+});
+router.get("/get-book-by-id/:id",async(req,res)=>{
+    try{ const{id}=req.params;
+        const book=await Book.findById(id);
+        return res.json({status:"Success",data:book,});
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:"An error occurred"})
+    }
+    
+});
 module.exports=router
