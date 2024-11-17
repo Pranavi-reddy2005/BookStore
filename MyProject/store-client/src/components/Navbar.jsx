259aad22ce78551a92602325730f5eb4cbfd285d
Pranavi-reddy2005 <ammireddypranavi35@gmail.com>
2024-11-18 02:38:15 +0530

    SignUp and LogIn and Implementing Roles

--- MyProject/store-client/src/components/Navbar.jsx
+++ MyProject/store-client/src/components/Navbar.jsx
@@ -45,27 +45,38 @@ const Navbar = () => {
       <div className='nav-links-bookheaven block md:flex items-center gap-4'>
         <div className='hidden md:flex gap-4'>
           {links.map((items, i) => (
-            <Link
+           <div className='flex items-center'>
+           {items.title === 'Profile' ?  <Link
+              to={items.link}
+              className='px-4 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300'
+              key={i}
+            >
+              {items.title}
+            </Link> :  <Link
               to={items.link}
               className='hover:text-blue-500 tranition-all duration-300'
               key={i}
             >
               {items.title}{' '}
             </Link>
+           }
+           </div>
           ))}
         </div>
-        <div className='hidden md:flex gap-4'>
-          <Link
-            to='/LogIn'
-            className='px-4 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300'>
-            LogIn
-          </Link>
-          <Link
-            to='/SignUp'
-            className='px-4 py-1  bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300'>
-            SignUp
-          </Link>
-          </div>
+       {isLoggedIn=== false && (
+         <div className='hidden md:flex gap-4'>
+         <Link
+           to='/LogIn'
+           className='px-4 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300'>
+           LogIn
+         </Link>
+         <Link
+           to='/SignUp'
+           className='px-4 py-1 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300'>
+           SignUp
+         </Link>
+         </div>
+       )}
         
         <button className='text-white text-2xl hover:text-zinc-400' onClick={() => 
           MobileNav === 'hidden'
@@ -93,16 +104,20 @@ const Navbar = () => {
             </Link>
           ))}
            
-          <Link
+         {isLoggedIn === false && (
+          <>
+           <Link
             to='/LogIn'
-            className={`${MobileNav} px-8 mb-8 text-3xl font-semibold py-2 border border-blue-500 rounded text-white hover:bg-white-800 hover:text-blue-800 transition-all duration-300`}>
+            className={`${MobileNav} px-8 mb-8 text-3xl font-semibold py-2 border border-blue-500 rounded text-white hover:bg-white hover:text-zinc-800 transition-all duration-300`}>
             LogIn
           </Link>
           <Link
             to='/SignUp'
-            className={`${MobileNav} px-8  mb-8 text-3xl font-semibold py-2  bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300`}>
+            className={`${MobileNav} px-8 mb-8 text-3xl font-semibold py-2 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300`}>
             SignUp
           </Link>
+          </>
+         )}
          
     </div>
     </>
